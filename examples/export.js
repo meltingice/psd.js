e08b2e46107734499ee89eb90243bf8fa2a5ed55
Ryan LeFevre <meltingice8917@gmail.com>
2014-05-15 07:45:12 -0400

    Fix package.json and add .npmignore

--- examples/export.js
+++ examples/export.js
@@ -1,5 +1,9 @@
 var PSD = require('../');
 
-psd = PSD.open('./examples/images/example.psd').then(function (psd) {
-  psd.image.saveAsPng('./output.png')
+var file = process.argv[2] || './examples/images/example.psd';
+var start = new Date();
+psd = PSD.open(file).then(function (psd) {
+  psd.image.saveAsPng('./output.png').then(function () {
+    console.log("Finished in " + ((new Date()) - start) + "ms");
+  });
 });
