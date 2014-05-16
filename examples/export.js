641347978335a722e7cde7155fa93957dfe3fec8
Ryan LeFevre <meltingice8917@gmail.com>
2014-05-16 08:36:46 -0400

    Do promises properly in example

--- examples/export.js
+++ examples/export.js
@@ -2,8 +2,9 @@ var PSD = require('../');
 
 var file = process.argv[2] || './examples/images/example.psd';
 var start = new Date();
-psd = PSD.open(file).then(function (psd) {
-  psd.image.saveAsPng('./output.png').then(function () {
-    console.log("Finished in " + ((new Date()) - start) + "ms");
-  });
-});
+
+PSD.open(file).then(function (psd) {
+  return psd.image.saveAsPng('./output.png');
+}).then(function () {
+  console.log("Finished in " + ((new Date()) - start) + "ms");
+});;
